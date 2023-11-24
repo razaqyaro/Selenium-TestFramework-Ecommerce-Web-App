7f3608a8beaf99f88407832b4feb9332bf222a47
Abdul-Razak  Hussein <abdul-razak.hussein@amalitech.com>
2023-11-24 09:03:44 +0000

    login to the app

---
+++ src/test/java/razak/StandAloneTest.java
@@ -0,0 +1,25 @@
+package razak;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class StandAloneTest
+{
+    private static String password = "razzy@gmail.comY$7";
+    private static String email = "razzy@gmail.com";
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.get("https://rahulshettyacademy.com/client");
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+        driver.findElement(By.id("userPassword")).sendKeys(password);
+        driver.findElement(By.id("login")).click();
+    }
+
+}
